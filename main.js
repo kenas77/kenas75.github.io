cb3b5845936ff57146bb7600c482c390417707fe
VanInd2002 <hanif.indradi@upi.edu>
2023-06-20 22:34:04 +0700

    20/6/2023 #13

--- main.js
+++ main.js
@@ -20,3 +20,51 @@ var content = document.getElementsByTagName('body')[0];
             image2.src = 'ASET/malem.png';
         }
         }
+
+window.addEventListener('scroll', function() {
+            var scrollPosition = window.scrollY;
+        
+            // Mengubah posisi gambar hiasan1 saat di-scroll
+            var hiasan1 = document.getElementById('hiasan1');
+            hiasan1.style.transform = 'translateX(' + -scrollPosition * 1 + 'px)';
+        
+            // Mengubah posisi gambar hiasan2 saat di-scroll
+            var hiasan2 = document.getElementById('hiasan2');
+            hiasan2.style.transform = 'translateX(' + scrollPosition * 1 + 'px)';
+});
+const testimonialContainer = document.querySelector(".testimonial-container");
+const testimonialItems = document.querySelectorAll(".testimonial-item");
+const prevButton = document.getElementById("prevButton");
+const nextButton = document.getElementById("nextButton");
+
+let currentIndex = 0;
+const slideAmount = 3;
+
+function showTestimonials() {
+  testimonialItems.forEach((item, index) => {
+    if (index >= currentIndex && index < currentIndex + slideAmount) {
+      item.style.display = "block";
+    } else {
+      item.style.display = "none";
+    }
+  });
+}
+
+function showNextTestimonials() {
+  if (currentIndex + slideAmount < testimonialItems.length) {
+    currentIndex++;
+    showTestimonials();
+  }
+}
+
+function showPreviousTestimonials() {
+  if (currentIndex > 0) {
+    currentIndex--;
+    showTestimonials();
+  }
+}
+
+prevButton.addEventListener("click", showPreviousTestimonials);
+nextButton.addEventListener("click", showNextTestimonials);
+
+showTestimonials();
